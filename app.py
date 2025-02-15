d036ba263664a4c2fb1e11a4320ba603e1f600ff
Yang Yifei <nexusyangsuibe@gmail.com>
2025-02-16 01:11:58 +0800

    some small modifications
    some small modifications

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ if __name__=="__main__":
     concat_df_cfg={
         "runtime_code":"cyj", # 运行时代码，用于区分不同的运行时，用于输出文件名的开头
         "data_source":"zip", # 数据来源，可选zip和folder，即从压缩文件中读入或从文件夹中读入
-        "target_folder":r"rawdata", # 待处理数据所在的文件夹，如果是zip文件也需要放在一个文件夹中
+        "target_folder":r"your_target_folder", # 待处理数据所在的文件夹，如果是zip文件也需要放在一个文件夹中
         "csv_delimiter":"\t", # csv分隔符，仅在读取csv文件时生效
         "usecols":"all", # 表格中要读取的列的列名，若指定则只读取指定列，若不指定则读取所有列，默认为all即读取所有列；指定的格式为list[str]或list[list[str],list[str],...]
         "ts_index_column_name":None, # 作为索引的时间序列的列名，若指定则会自动转为pd.DateTimeIndex格式并升序排列，若不指定则不会转换时间序列格式，默认为None即不指定，指定的格式为str
         "skiprows":[1,2], # 读取表格时要跳过的行，从0开始计数。从CSMAR下载的数据一般需要跳过[1,2]，即中文表名列与单位列；从CNRDS下载的数据一般需要跳过[1]，默认为None即不指定
         "convert_str_columns":"auto", # 需要被强制转为字符串类型的列的列名，以防止字符串被当作其他格式处理，可以选择auto根据列名是否以id,cd,code,symbol开头或结尾自动判断，或者以list[str]或list[list[str],list[str],...]格式手动指定列名，不需要转换则设为空列表或None
-        "output_filename":"test.pkl", # 表格合并程序输出文件名，为防止文本中逗号与逗号分隔符混淆不支持csv，支持xlsx，数据量较大时会以1000000行为分割输出多个excel文件，支持pickle(.pkl)格式
+        "output_filename":"your_output_filename.pkl/xlsx", # 表格合并程序输出文件名，为防止文本中逗号与逗号分隔符混淆不支持csv，支持xlsx，数据量较大时会以1000000行为分割输出多个excel文件，支持pickle(.pkl)格式
         "zip_starts_with":"all", # 压缩文件名开头，当target_folder中有多种数据时可以据此选择性读入想要的，默认为"all"，当定义为列表或元组时会根据共同列横向合并多个表格，仅在data_source为zip时有效
         "filter_conditions":None, # 筛选条件，仅用于简单的筛选操作，复杂的筛选操作请在结果输出后自行处理，不需要可以设为空字典或None，不同筛选条件之间的关系是逻辑与，运行时筛选的好处是可以减少内存占用
         # "filter_conditions":{ # 筛选条件的定义格式
@@ -36,8 +36,7 @@ if __name__=="__main__":
         #         },
         #     ],
         #     "not_allow_nan_columns":["NewsContent"], # 不允许有缺失值的列，删去定义的列中有空缺值的观测，None为不删去任何观测，"all"为删去所有有空缺值的观测
-        #     # "not_str_filter_conditions":None, # 非字符串筛选条件，不需要可以设为空列表或None
-        #     # "str_filter_conditions":None, # 字符串筛选条件，不需要可以设为空列表或None
+        #     "str_filter_conditions":None, # 字符串筛选条件，不需要可以设为空列表或None
         # },
         # 以下两项respwanpoint文件夹配置，respawnpoint文件夹用于存储临时文件，以避免在读取大量数据时占用过多内存造成程序崩溃，若不清空respawnpoint文件夹可能导致之前的临时文件被重复读入
         "clear_respawnpoint_before_run":True, # 开始运行前是否清空respawnpoint文件夹
@@ -45,12 +44,12 @@ if __name__=="__main__":
     }
   
     print("程序运行开始\n")
-    t0=time.perf_counter()
+    t0=time.time()
     if "respawnpoint" not in os.listdir():
         print(f"在工作目录{os.getcwd()}下未找到用于存储临时文件的respawnpoint文件夹，将自动创建")
         os.mkdir("respawnpoint")
     if "finalresults" not in os.listdir():
         print(f"在工作目录{os.getcwd()}下未找到用于存储最终结果的finalresults文件夹，将自动创建")
         os.mkdir("finalresults")
-    rtn_catdf=concatDF(concat_df_cfg["runtime_code"],concat_df_cfg["data_source"],concat_df_cfg["target_folder"],concat_df_cfg["usecols"],concat_df_cfg["ts_index_column_name"],concat_df_cfg["filter_conditions"],concat_df_cfg["csv_delimiter"],concat_df_cfg["convert_str_columns"],concat_df_cfg["output_filename"],concat_df_cfg["skiprows"],concat_df_cfg["zip_starts_with"],concat_df_cfg["clear_respawnpoint_before_run"],concat_df_cfg["clear_respawnpoint_upon_conplete"])
-    print("\n程序运行完成，用时{:.4f}秒".format(time.perf_counter()-t0))
+    concatDF(concat_df_cfg["runtime_code"],concat_df_cfg["data_source"],concat_df_cfg["target_folder"],concat_df_cfg["usecols"],concat_df_cfg["ts_index_column_name"],concat_df_cfg["filter_conditions"],concat_df_cfg["csv_delimiter"],concat_df_cfg["convert_str_columns"],concat_df_cfg["output_filename"],concat_df_cfg["skiprows"],concat_df_cfg["zip_starts_with"],concat_df_cfg["clear_respawnpoint_before_run"],concat_df_cfg["clear_respawnpoint_upon_conplete"])
+    print(f"\n程序运行完成，用时{(time.time()-t0):.4f}秒") # It is very weird that this line will not conduct until waiting for a very long period (about 10min) and the memory usage holds at a very high level. It seems to be a problem triggered by lingered subprocesses, but when I use breakpoint debug to observe, the problem disappeared.
